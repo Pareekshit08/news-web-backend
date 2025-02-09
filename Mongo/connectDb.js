30d9d6f156e6102d225d90ddd74db75b24ea56f8
Pareekshit08 <pareekshit.s08@gmail.com>
2025-02-09 19:45:00 +0530

    09/02/2025

--- Mongo/connectDb.js
+++ Mongo/connectDb.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
-const url = "mongodb://127.0.0.1:27017/News";
-// const url = "mongodb+srv://pareekshits08:m0luC8Jj7aDxT3b5@cluster0.8nadw.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0/News";
+// const url = "mongodb://127.0.0.1:27017/News";
+const url = "mongodb+srv://pareekshits08:m0luC8Jj7aDxT3b5@cluster0.8nadw.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0/News";
 
 const UserSchema = mongoose.Schema({
     name:String,
